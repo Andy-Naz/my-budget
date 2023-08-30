2ab64ca4e0fd4520a8335095a022d142240d366f
Andrey Nazarenko <skiminok86@gmail.com>
2023-08-30 16:16:57 +0300

    add mockData to FireBase

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,13 +1,21 @@
 import React, { useEffect } from "react"
 import Widget from "../components/Widget"
 import API from "../api"
+import useMockData from "../utils/mockData"
 
 const MainPage = () => {
     useEffect(() => {
         API.accounts.fetchAll().then((data) => console.log(data))
     }, [])
+    const { error, initialize } = useMockData()
+    const handleClick = () => {
+        initialize()
+    }
     return (
         <>
+            <button className="btn btn-primary" onClick={handleClick}>
+                Инициализировать
+            </button>
             <div className="container mt-3">
                 <div className="row align-items-start">
                     <div className="col mb-3">
---
+++ src/store/createStore.js
@@ -0,0 +1,18 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit"
+import qualitiesReducer from "./qualities"
+import professionsReducer from "./transactions"
+import usersReducer from "./users"
+import commentsReducer from "./comments"
+
+const rootReducer = combineReducers({
+    qualities: qualitiesReducer,
+    professions: professionsReducer,
+    users: usersReducer,
+    comments: commentsReducer
+})
+
+export function createStore() {
+    return configureStore({
+        reducer: rootReducer
+    })
+}
