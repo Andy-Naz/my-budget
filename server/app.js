2194446f0d34d3088f32f4057ddaa29a703dc514
Andrey Nazarenko <skiminok86@gmail.com>
2023-09-17 20:00:32 +0300

    connect MongoDB

--- server/app.js
+++ server/app.js
@@ -2,3 +2,29 @@ const express = require("express")
 const mongoose = require("mongoose")
 const config = require("config")
 const chalk = require("chalk")
+
+//andrey
+//SymzsTpWUaKM4cps
+
+const app = express()
+
+const PORT = config.get("port") ?? 8080
+
+async function start() {
+    try {
+        // mongoose.connection.once("open", () => {
+        //     initDatabase()
+        // })
+
+        await mongoose.connect(config.get("mongoUri"))
+        console.log(chalk.bgBlue("MongoDB connected"))
+        app.listen(PORT, () => {
+            console.log(chalk.bgGreen(`Server has been started on port ${PORT}...`))
+        })
+    } catch (error) {
+        console.log(chalk.red(error.message))
+        process.exit(1)
+    }
+}
+
+start()
