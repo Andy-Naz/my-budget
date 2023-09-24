8fb0aab834baaab4a7f08dda138bc2eeee50405c
Andrey Nazarenko <skiminok86@gmail.com>
2023-09-25 01:57:59 +0300

    add logo and favicon

--- client/src/components/ui/NavBar.jsx
+++ client/src/components/ui/NavBar.jsx
@@ -5,6 +5,7 @@ import { getIsLoggedIn } from "../../store/users"
 import NavProfile from "./NavProfile"
 import { Disclosure, Menu } from "@headlessui/react"
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline"
+import Logo from "../../assets/images/logo.png"
 
 const navigationList = [
     { name: "Дашборд", to: "/", current: true },
@@ -50,11 +51,7 @@ const NavBar = () => {
                             </div>
                             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                                 <div className="flex flex-shrink-0 items-center">
-                                    <img
-                                        className="h-8 w-auto"
-                                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                                        alt="Your Company"
-                                    />
+                                    <img className="h-6 w-auto hover:drop-shadow-lg" src={Logo} alt="Your Company" />
                                 </div>
                                 <div className="hidden sm:ml-6 sm:block">
                                     <div className="flex space-x-4">
