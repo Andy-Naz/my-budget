f4e684df5c34cb4e0aa3c082eed62503ef4b8144
Andrey Nazarenko <skiminok86@gmail.com>
2023-09-25 23:33:57 +0300

    refactoring edit transaction form

--- client/src/components/ui/Pagination.jsx
+++ client/src/components/ui/Pagination.jsx
@@ -19,18 +19,19 @@ const Pagination = ({ itemsCount, pageSize, onPageChange, currentPage }) => {
             {pageCount > 1 && (
                 <div className="flex items-center justify-between px-4 py-3 sm:px-6">
                     <div className="flex flex-1 justify-between sm:hidden">
-                        <a
-                            href="#"
+                        <button
                             className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+                            onClick={() => onPageChange(currentPage === 1 ? currentPage : currentPage - 1)}
                         >
                             Предыдущий
-                        </a>
-                        <a
+                        </button>
+                        <button
                             href="#"
                             className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+                            onClick={() => onPageChange(currentPage === pages.length ? currentPage : currentPage + 1)}
                         >
                             Следующий
-                        </a>
+                        </button>
                     </div>
                     <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
                         <div>
--- client/src/components/ui/transaction/EditTransaction.jsx
+++ client/src/components/ui/transaction/EditTransaction.jsx
@@ -87,52 +87,78 @@ const EditTransaction = () => {
         const categoriesList = categories.map((category) => ({ label: category.name, value: category._id }))
 
         return (
-            <div className="container mt-5">
-                <div className="row">
-                    <div className="col-md-6 offset-md-3 shadow p-4">
-                        <h3>Редактировать транзакцию</h3>
-                        <form onSubmit={handleSubmit}>
-                            <SelectField
-                                label="Счет"
-                                defaultOption="Выбрать из списка..."
-                                options={accountsList}
-                                name="account"
-                                onChange={handleChange}
-                                value={data.account}
-                                error={errors.account}
-                            />
-                            <SelectField
-                                label="Категория"
-                                defaultOption="Выбрать из списка..."
-                                options={categoriesList}
-                                name="category"
-                                onChange={handleChange}
-                                value={data.category}
-                                error={errors.category}
-                            />
-                            <TextField
-                                label="Сумма"
-                                name="amount"
-                                type="number"
-                                value={data.amount}
-                                onChange={handleChange}
-                                error={errors.amount}
-                            />
-                            <TextAreaField
-                                value={data.comment || ""}
-                                onChange={handleChange}
-                                name="comment"
-                                label="Комментарий"
-                                error={errors.comment}
-                            />
-
-                            <button className="btn btn-primary w-100 mx-auto" type="submit" disabled={!isValid}>
-                                Submit
-                            </button>
-                        </form>
+            <>
+                <div className="w-full lg:w-8/12 px-4 mx-auto pt-10">
+                    <div className="px-4 sm:px-0">
+                        <h3 className="text-base font-semibold leading-7 text-gray-900">Редактирование транзакции</h3>
+                        <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
+                            Скорректируйте необходимую информацию
+                        </p>
                     </div>
+                    <form onSubmit={handleSubmit}>
+                        <div className="mt-6 border-t border-gray-100">
+                            <dl className="divide-y divide-gray-100">
+                                <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                                    <dt className="text-sm font-medium leading-6 text-gray-900">Счет</dt>
+                                    <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                                        <SelectField
+                                            defaultOption="Выбрать из списка..."
+                                            options={accountsList}
+                                            name="account"
+                                            onChange={handleChange}
+                                            value={data.account}
+                                            error={errors.account}
+                                        />
+                                    </dd>
+                                </div>
+                                <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                                    <dt className="text-sm font-medium leading-6 text-gray-900">Категория</dt>
+                                    <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                                        <SelectField
+                                            defaultOption="Выбрать из списка..."
+                                            options={categoriesList}
+                                            name="category"
+                                            onChange={handleChange}
+                                            value={data.category}
+                                            error={errors.category}
+                                        />
+                                    </dd>
+                                </div>
+                                <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                                    <dt className="text-sm font-medium leading-6 text-gray-900">Сумма</dt>
+                                    <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                                        <TextField
+                                            name="amount"
+                                            type="number"
+                                            value={data.amount}
+                                            onChange={handleChange}
+                                            error={errors.amount}
+                                        />
+                                    </dd>
+                                </div>
+                                <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                                    <dt className="text-sm font-medium leading-6 text-gray-900">Комментарий</dt>
+                                    <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                                        <TextAreaField
+                                            value={data.comment || ""}
+                                            onChange={handleChange}
+                                            name="comment"
+                                            error={errors.comment}
+                                        />
+                                    </dd>
+                                </div>
+                            </dl>
+                        </div>
+                        <button
+                            type="submit"
+                            disabled={!isValid}
+                            className="flex w-full sm:w-2/12 justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                            Сохранить
+                        </button>
+                    </form>
                 </div>
-            </div>
+            </>
         )
     }
     return "Loading..."
