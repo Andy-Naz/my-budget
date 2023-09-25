a2380f36fe40aabf6a14e256d2d870252f1f1344
Andrey Nazarenko <skiminok86@gmail.com>
2023-09-25 18:10:24 +0300

    fix transform date bug

--- client/src/components/ui/DateTransaction.jsx
+++ client/src/components/ui/DateTransaction.jsx
@@ -3,14 +3,14 @@ import React from "react"
 const DateTransaction = ({ data }) => {
     const date = new Date(data.created_at)
     const year = date.getFullYear()
-    let month = date.getMonth()
-    let day = date.getDay()
+    let month = date.getMonth() + 1
+    let day = date.getDate()
     if (day < 10) {
         day = `0${day}`
     }
 
     if (month < 10) {
-        month = `0${month + 1}`
+        month = `0${month}`
     }
     const formatDate = `${day}.${month}.${year}`
     return <p>{formatDate}</p>
--- client/src/components/ui/transaction/TransactionAddButton.jsx
+++ client/src/components/ui/transaction/TransactionAddButton.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom"
 const TransactionAddButton = () => {
     return (
         <Link to="/transaction">
-            <button className="hidden sm:block flex mr-2 w-24 h-8 justify-center rounded-md bg-indigo-600 p-1 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            <button className="hidden sm:block mr-2 w-24 h-8 justify-center rounded-md bg-indigo-600 p-1 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                 + Создать
             </button>
-            <button className="sm:hidden flex mr-2 w-8 h-8 justify-center rounded-md bg-indigo-600 p-1 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            <button className="sm:hidden mr-2 w-8 h-8 justify-center rounded-md bg-indigo-600 p-1 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                 +
             </button>
         </Link>
