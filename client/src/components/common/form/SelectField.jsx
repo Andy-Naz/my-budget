5ed653d30e9b81a037183208a1fcfa5cf732c674
Andrey Nazarenko <skiminok86@gmail.com>
2023-10-06 00:30:03 +0300

    plus select field

--- client/src/components/common/form/SelectField.jsx
+++ client/src/components/common/form/SelectField.jsx
@@ -20,7 +20,7 @@ const SelectField = ({ label, value, onChange, defaultOption, options, name, err
                     name={name}
                     value={value}
                     onChange={handleChange}
-                    className="block w-full rounded-md border-1 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-1 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 >
                     <option disabled value="">
                         {defaultOption}
